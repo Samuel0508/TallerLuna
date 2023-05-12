9ffeefeea33880df8bc0e4635cbddd5a45767881
Samuel <Samuel@DESKTOP-7DBK8FL>
2023-05-12 16:19:10 -0600

    Agregar archivos de proyecto.

---
+++ TallerLuna/Controllers/PagesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TallerLuna.Controllers
+{
+    public class PagesController : Controller
+    {
+        public IActionResult agenda()
+        {
+            return View();
+        }
+
+        public IActionResult clientes()
+        {
+            return View();
+        }
+
+        public IActionResult facturacion()
+        {
+            return View();
+        }
+
+        public IActionResult gastos()
+        {
+            return View();
+        }
+
+        public IActionResult ingresos()
+        {
+            return View();
+        }
+        public IActionResult inventario()
+        {
+            return View();
+        }
+        public IActionResult reservar()
+        {
+            return View();
+        }
+        public IActionResult servicio()
+        {
+            return View();
+        }
+
+    }
+}
